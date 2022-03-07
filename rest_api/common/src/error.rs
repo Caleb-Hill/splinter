fb8c82a3898ec9ea13e8f53b6be3fbecd0ce65e3
Caleb Hill <hill@bitwise.io>
2022-03-07 12:42:09 -0600

    Add error module to common
    This error type is ment to directly map to HTTP status codes and be used
    by the resources contained in `resources`.
    
    Any actix version that relies on resources using these error types is
    going to need to implement their own error type as an intermediary.
    Actix-web has traits for converting errors into responses but because of
    rusts foreign implementation requirements each crate can not
    implement those traits for this type.
    
    Signed-off-by: Caleb Hill <hill@bitwise.io>

---
+++ rest_api/common/src/error.rs
@@ -0,0 +1,60 @@
+// Copyright 2018-2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::error::Error;
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum ResponseError {
+    BadRequest(String),
+    NotFound(String),
+    InternalError(String, Option<Box<dyn Error>>),
+    NotAuthorized,
+}
+
+impl std::error::Error for ResponseError {}
+
+impl Display for ResponseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ResponseError::BadRequest(msg) => write!(f, "Bad Request: {}", msg),
+            ResponseError::NotFound(url) => write!(f, "Could not find resource for: {}", url),
+            ResponseError::InternalError(msg, Some(err)) => {
+                write!(f, "Internal Error: {}: {}", msg, err)
+            }
+            ResponseError::InternalError(msg, None) => write!(f, "Internal Error: {}", msg),
+            ResponseError::NotAuthorized => write!(f, "Not Authorized"),
+        }
+    }
+}
+
+impl ResponseError {
+    pub fn bad_request<S>(msg: S) -> Self
+    where
+        S: Into<String>,
+    {
+        Self::BadRequest(msg.into())
+    }
+
+    pub fn not_found(url: String) -> Self {
+        Self::NotFound(url)
+    }
+
+    pub fn internal_error<S>(msg: S, err: Option<Box<dyn Error>>) -> Self
+    where
+        S: Into<String>,
+    {
+        Self::InternalError(msg.into(), err)
+    }
+}
