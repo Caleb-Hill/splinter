d94f90dbf403c890e9b9d46147da9a2712601fa9
Caleb Hill <hill@bitwise.io>
2022-03-09 12:13:01 -0600

    Non functional version of get_admin_circuits
    foreign type impls are the issue here. This should get fixuped into the
    "add get_admin_circuits mod to actix_web_4" commit.

--- rest_api/actix_web_4/src/admin/get_admin_circuits.rs
+++ rest_api/actix_web_4/src/admin/get_admin_circuits.rs
@@ -11,3 +11,73 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+
+use std::collections::HashMap;
+use std::convert::TryFrom;
+use std::sync::{Arc,Mutex};
+
+use splinter::store::StoreFactory;
+use splinter_rest_api_common::resources::admin::get_admin_circuits::{v1, v2};
+
+use actix_web::body::BoxBody;
+use actix_web::{HttpRequest, HttpResponse, Responder};
+
+use crate::error::RestError;
+use crate::request::RequestWrapper;
+
+use crate::protocol_version::{
+    ProtocolVersion, MAX_PROTOCOL_VERSION, MIN_PROTOCOL_VERSION,
+};
+
+pub async fn get_admin_circuits(request: HttpRequest) -> Result<HttpResponse<BoxBody>, RestError> {
+    let store = request.app_data::<Arc<Mutex<Box<dyn StoreFactory + Send >>>>()
+        .ok_or_else(|| RestError::InternalError("Could not get store factory from app".into(),None))?
+        .lock().unwrap().get_admin_service_store();
+    match ProtocolVersion::try_from(&request) {
+        Ok(system_version) => match system_version.into() {
+            MIN_PROTOCOL_VERSION..=1 =>{
+                let args: v1::Arguments = v1::Arguments::new(RequestWrapper::from(&request))?;
+                let response: V1Response = v1::get_admin_circuits(args,store)?.into();
+                Ok(response.respond_to(&request))
+            }
+            2..=MAX_PROTOCOL_VERSION =>{
+                let args: v2::Arguments = v2::Arguments::new(RequestWrapper::from(&request))?;
+                let response: V2Response = v2::get_admin_circuits(args,store)?.into();
+                Ok(response.respond_to(&request))
+            }
+            // this should be unreachable as ProtocolVersion does the check
+            _ => Err(RestError::BadRequest(
+                "Protocol version does not have a mapped resource version".to_string()
+            )),
+        },
+        Err(_) => Ok(HttpResponse::Ok().body("Could not get resource")),
+    }
+}
+
+
+impl Responder for v1::Response {
+    type Body = BoxBody;
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        HttpResponse::Ok().json(self)
+    }
+}
+
+struct V2Response {
+    inner: v2::Response,
+}
+
+impl From<v2::Response> for V2Response {
+    fn from(inner: v2::Response) -> Self {
+        Self {
+            inner
+        }
+    }
+}
+
+impl Responder for V2Response {
+    type Body = BoxBody;
+    fn respond_to(self, _req: &HttpRequest) -> HttpResponse<Self::Body> {
+        HttpResponse::Ok().json(self)
+    }
+}
