613fb1ba2cdacb00b97139a1c9db890ff3028ae4
Caleb Hill <hill@bitwise.io>
2022-03-16 14:52:18 -0500

    Add request module to actix_web_4 crate

---
+++ rest_api/actix_web_4/src/request.rs
@@ -0,0 +1,62 @@
+// Copyright 2018-2022 Cargill Incorporated
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::collections::HashMap;
+use std::convert::From;
+
+use actix_web::web::Query;
+use actix_web::HttpRequest;
+use splinter_rest_api_common::request::Request;
+
+pub struct RequestWrapper<'a> {
+    inner: &'a HttpRequest,
+}
+
+impl Request for RequestWrapper<'_> {
+    fn uri(&self) -> &str {
+        self.inner.uri().path()
+    }
+
+    fn get_header_value(&self, key: &str) -> Option<Vec<u8>> {
+        self.inner
+            .head()
+            .headers()
+            .get(key)
+            .map(|a| a.as_bytes().into())
+    }
+
+    fn get_header_values(&self, key: &str) -> Box<dyn Iterator<Item = Vec<u8>>> {
+        let headers: Vec<Vec<u8>> = self
+            .inner
+            .head()
+            .headers()
+            .get_all(key)
+            .map(|a| a.as_bytes().into())
+            .collect::<Vec<_>>();
+        Box::new(headers.into_iter())
+    }
+
+    fn get_query_value(&self, key: &str) -> Option<String> {
+        match Query::<HashMap<String, String>>::from_query(self.inner.query_string()) {
+            Ok(map) => map.get(key).map(|a| a.to_owned()),
+            Err(_) => None,
+        }
+    }
+}
+
+impl<'a> From<&'a HttpRequest> for RequestWrapper<'a> {
+    fn from(inner: &'a HttpRequest) -> Self {
+        Self { inner }
+    }
+}
